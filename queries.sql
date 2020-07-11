83a791a15979101faa9422c118bd40140cf84125
cathy <cathy.pattie@gmail.com>
2020-07-10 20:11:55 -0500

    All the updates

--- queries.sql
+++ queries.sql
@@ -1,7 +1,11 @@
 ## Part 1: Test it with SQL
+describe techjobs.job;
 
 ## Part 2: Test it with SQL
+SELECT * FROM techjobs.employer AS e WHERE e.location LIKE 'St. Louis' OR e.location='St. Louis City';
 
 ## Part 3: Test it with SQL
+DROP TABLE techjobs.jobs
 
 ## Part 4: Test it with SQL
+SELECT DISTINCT s.id, s.name, s.description FROM techjobs.skill s INNER JOIN techjobs.job_skills AS js WHERE s.id = js.skills_id ;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,32 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    private String location;
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
